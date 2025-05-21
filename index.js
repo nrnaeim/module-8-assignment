e06d0370adf20a9c56c64a2e5a999ebb4cdb5e48
Niemur Rahman <nrnaeim@gmail.com>
2025-05-22 04:51:16 +0600

    different type of functionality added

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const display = document.querySelector(".display input");
-//display handler
+
+//display clear button handler
+document
+  .querySelector(".clearBtn")
+  .addEventListener("click", () => (display.value = "0"));
+
+//input handler
 document.querySelectorAll(".inputBtn").forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    e.preventDefault();
     const clickedBtn = e.target.innerText;
     let displayValue = display.value;
 
-    if (displayValue === "" && ["+", "-", "*", "/"].includes(clickedBtn)) {
+    if (
+      displayValue === "0" &&
+      ["+", "-", "*", "/", "%", "0"].includes(clickedBtn)
+    ) {
       //no code for if display is empty and first clicked operator
+    } else if (displayValue === "0" && clickedBtn === ".") {
+      display.value = displayValue + clickedBtn;
+    } else if (displayValue === "0") {
+      display.value = clickedBtn;
     } else {
       let lastChar = displayValue[displayValue.length - 1];
       if (
-        ["+", "-", "*", "/"].includes(lastChar) &&
-        ["+", "-", "*", "/"].includes(clickedBtn)
+        ["+", "-", "*", "/", "%", "."].includes(lastChar) &&
+        ["+", "-", "*", "/", "%", "."].includes(clickedBtn)
       ) {
         //no code if last character is an operator
       } else {
@@ -21,11 +33,30 @@ document.querySelectorAll(".inputBtn").forEach((btn) => {
     }
   });
 });
+//delete button handler
+document.querySelector(".deleteBtn").addEventListener("click", () => {
+  const displayValue = display.value;
+  if (displayValue.length === 1) {
+    display.value = "0";
+  } else {
+    display.value = displayValue.slice(0, displayValue.length - 1);
+  }
+});
 
-//clear button handler
-document
-  .querySelector(".clear")
-  .addEventListener("click", () => (display.value = ""));
+//percent btn handler
+document.querySelector(".percentBtn").addEventListener("click", () => {
+  const displayValue = display.value;
+  for (let i = displayValue.length - 1; i >= 0; i--) {
+    if (["+", "-", "/", "*"].includes(displayValue[i])) {
+      const indexOfOperator = displayValue.lastIndexOf(displayValue[i]);
+      const evaluatePart = displayValue.slice(0, indexOfOperator + 1);
+      const percentagePart = displayValue.slice(indexOfOperator + 1);
+      const newString = `${evaluatePart}${Number(percentagePart) / 100}`;
+      display.value = newString;
+      break;
+    }
+  }
+});
 
 //evaluate or = button handler
 document.querySelector(".evaluate").addEventListener("click", () => {
@@ -53,4 +84,24 @@ document.querySelector("#closeModal").addEventListener("click", () => {
   document.querySelector(".calculator").style.display = "node";
   document.querySelector(".modalOpener").style.display = "flex";
   display.value = "";
+});
+
+//on off button handler
+document.querySelector(".onOffBtn").addEventListener("click", (e) => {
+  if (e.target.textContent === "ON") {
+    e.target.textContent = "OFF";
+    display.value = "0";
+  } else {
+    e.target.textContent = "ON";
+    display.value = "";
+  }
+  document.querySelectorAll(".btn").forEach((btn) => {
+    if (!btn.classList.contains("onOffBtn")) {
+      if (btn.disabled) {
+        btn.disabled = false;
+      } else {
+        btn.disabled = true;
+      }
+    }
+  });
 });
